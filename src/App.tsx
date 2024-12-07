889f5535624e557385e1e3ad0865868667973c70
Zakeer Hussain <zakeer@zakeer.me>
2024-12-07 06:42:37 +0000

    localstorage added for form

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import RegisterForm from './components/auth/RegisterForm';
 import LoginForm from './components/auth/LoginForm';
 
+const PREVIOUS_LOGGED_VIEW = localStorage.getItem("LOGGED_VIEW")
+
 function App() {
-  const [forRegister, toggleRegisterView] = useState(true); // [value, setFn]
+  const [forRegister, toggleRegisterView] = useState(PREVIOUS_LOGGED_VIEW === "true"); // [value, setFn]
 
   const toggleView = function () {
     // toggleRegisterView(false);
     toggleRegisterView(function (prevValue) {
-      return !prevValue;
+      const changedValue = !prevValue;
+      // localStorage.setItem("LOGGED_VIEW", String(changedValue));
+      return changedValue;
     });
   };
+
+  useEffect(() => {
+    localStorage.setItem("LOGGED_VIEW", String(forRegister));
+  }, [forRegister]);
 
   return (
     <>
